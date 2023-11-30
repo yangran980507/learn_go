1e20908c71919d4e69a45d874190a352f69d5269
yangran <1273444129@qq.com>
2023-11-30 21:57:34 +0800

    first linux

---
+++ gohub/app/http/controllers/api/v1/auth/signup_controller.go
@@ -0,0 +1,46 @@
+// Package auth 处理用户身份认证相关逻辑
+package auth
+
+import (
+	"gohub/app/http/controllers/api/v1"
+	"gohub/app/models/user"
+	"gohub/app/requests"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SignupController 注册控制器
+type SignupController struct {
+	v1.BaseAPIController
+}
+
+// IsPhoneExist 检测手机号是否被注册
+func (sc *SignupController) IsPhoneExist(c *gin.Context) {
+
+	// 获取请求参数，并做表单验证
+	request := &requests.SignupPhoneExistRequest{}
+	if ok := requests.Validate(c, request, requests.ValidateSignupPhoneExist); !ok {
+		return
+	}
+
+	//  检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsPhoneExist(request.Phone),
+	})
+}
+
+// IsEmailExist 检测邮箱是否已注册
+func (sc *SignupController) IsEmailExist(c *gin.Context) {
+
+	// 获取请求参数，并做表单验证
+	request := &requests.SignupEmailExistRequest{}
+	if ok := requests.Validate(c, request, requests.ValidateSignupEmailExist); !ok {
+		return
+	}
+
+	//  检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsEmailExist(request.Email),
+	})
+}

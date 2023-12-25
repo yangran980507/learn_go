4ae16f0dc148955e9e9ae6f2f54766efe84e6cfb
yangran <1273444129@qq.com>
2023-12-25 21:13:06 +0800

    Email + 邮件验证码注册

--- gohub/app/requests/signup_request.go
+++ gohub/app/requests/signup_request.go
@@ -56,7 +56,7 @@ func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]str
 	return validate(data, rules, messages)
 }
 
-// SignupUsingPhoneRequest 通过注册的请求信息
+// SignupUsingPhoneRequest 通过手机注册的请求信息
 type SignupUsingPhoneRequest struct {
 	Phone           string `json:"phone,omitempty" valid:"phone"`
 	VerifyCode      string `json:"verify_ode,omitempty" valid:"verify_code"`
@@ -103,6 +103,60 @@ func SignupUsingPhone(data interface{}, c *gin.Context) map[string][]string {
 	_data := data.(*SignupUsingPhoneRequest)
 	errs = validators.ValidatePasswordConfirm(_data.Password, _data.PasswordConfirm, errs)
 	errs = validators.ValidateVerifyCode(_data.Phone, _data.VerifyCode, errs)
+
+	return errs
+}
+
+// SignupUsingEmailRequest 通过邮箱注册的请求信息
+type SignupUsingEmailRequest struct {
+	Email           string `json:"email,omitempty" valid:"email"`
+	VerifyCode      string `json:"verify_ode,omitempty" valid:"verify_code"`
+	Name            string `json:"name" valid:"name"`
+	Password        string `json:"password,omitempty" valid:"password"`
+	PasswordConfirm string `json:"password_confirm,omitempty" valid:"password_confirm"`
+}
+
+func SignupUsingEmail(data interface{}, c *gin.Context) map[string][]string {
+
+	rules := govalidator.MapData{
+		"email":            []string{"required", "min:4", "max:30", "not_exists:users,email"},
+		"verify_code":      []string{"required", "digits:6"},
+		"name":             []string{"required", "alpha_num", "between:3,20", "not_exists:users,phone"},
+		"password":         []string{"required", "min:6"},
+		"password_confirm": []string{"required"},
+	}
+
+	messages := govalidator.MapData{
+		"email": []string{
+			"required:Email 为必填项",
+			"min:Email 长度需大于 4",
+			"max:Email 长度需小于 30",
+			"email:Email 格式不正确，请提供有效的邮箱地址",
+			"not_exists:Email 已被占用",
+		},
+		"verify_code": []string{
+			"required:验证码为必填",
+			"digits:验证码长度必须为 6 位的数字",
+		},
+		"name": []string{
+			"required:用户名为必填",
+			"alpha_num:用户名格式错误，只允许数字和英文",
+			"between:用户名长度需在 3～20 之间",
+		},
+		"password": []string{
+			"required:密码为必填",
+			"min:密码长度需大于 6",
+		},
+		"password_confirm": []string{
+			"required:确认密码框为必填",
+		},
+	}
+
+	errs := validate(data, rules, messages)
+
+	_data := data.(*SignupUsingEmailRequest)
+	errs = validators.ValidatePasswordConfirm(_data.Password, _data.PasswordConfirm, errs)
+	errs = validators.ValidateVerifyCode(_data.Email, _data.VerifyCode, errs)
 
 	return errs
 }

53cdfe91837f2601ed116bc5e8f3a178e7d64153
yangran <1273444129@qq.com>
2024-02-24 22:47:08 +0800

    factory 和 Faker

---
+++ gohub/database/factories/user_factory.go
@@ -0,0 +1,27 @@
+// Package factories 存放工厂方法
+package factories
+
+import (
+	"gohub/app/models/user"
+	"gohub/pkg/helpers"
+
+	"github.com/bxcodec/faker/v3"
+)
+
+func MakeUsers(times int) []user.User {
+	var objs []user.User
+
+	//设置唯一值
+	faker.SetGenerateUniqueValues(true)
+
+	for i := 0; i < times; i++ {
+		model := user.User{
+			Name:     faker.Username(),
+			Email:    faker.Email(),
+			Phone:    helpers.RandomNumber(11),
+			Password: "$2a$14$oPzVkIdwJ8KqY0erYAYQxOuAAlbI/sFIsG0R4MPc.3JbWWSuaUe",
+		}
+		objs = append(objs, model)
+	}
+
+}

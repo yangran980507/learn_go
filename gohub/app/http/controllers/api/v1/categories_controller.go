e580d75ffa506acdf7751d9a11c00ff1f2eb5c4d
yangran <1273444129@qq.com>
2024-02-26 23:07:12 +0800

    分类列表

--- gohub/app/http/controllers/api/v1/categories_controller.go
+++ gohub/app/http/controllers/api/v1/categories_controller.go
@@ -56,3 +56,17 @@ func (ctrl *CategoriesController) Update(c *gin.Context) {
 		response.Abort500(c)
 	}
 }
+
+func (ctrl *CategoriesController) Index(c *gin.Context) {
+	//表单验证
+	request := requests.CategoryRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
+
+	data, pager := category.Paginate(c, 10)
+	response.JSON(c, gin.H{
+		"data":  data,
+		"pager": pager,
+	})
+}

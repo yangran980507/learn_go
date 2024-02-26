8b3c16640b419147eef07d7c1cd63b1e73371d33
yangran <1273444129@qq.com>
2024-02-26 22:35:11 +0800

    更新分类

--- gohub/routes/api.go
+++ gohub/routes/api.go
@@ -71,6 +71,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			cgcGroup := v1.Group("/categories")
 			{
 				cgcGroup.POST("", middlewares.AuthJWT(), cgc.Store)
+				cgcGroup.PUT("/:id", middlewares.GuestJWT(), cgc.Update)
 			}
 		}
 	}
